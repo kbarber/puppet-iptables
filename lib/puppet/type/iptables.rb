9a46d35148fffc23ecd7db90d325b143052eabfb
Ruben Kerkhof <ruben@tilaa.nl>
2011-08-30 13:08:57 +0200

    Make module work on Scientific Linux

--- lib/puppet/type/iptables.rb
+++ lib/puppet/type/iptables.rb
@@ -468,7 +468,7 @@ module Puppet
           # Run iptables save to persist rules. The behaviour is to do nothing
           # if we no nothing of the operating system.
           persist_cmd = case Facter.value(:operatingsystem).downcase
-            when "fedora", "redhat", "centos"
+            when "fedora", "redhat", "centos", "scientific"
               then "/sbin/service iptables save"
             when "ubuntu", "debian"
               then "/sbin/iptables-save > /etc/iptables.rules"
